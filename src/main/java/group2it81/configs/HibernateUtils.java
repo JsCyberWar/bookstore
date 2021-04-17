1f83a91cc5d323dfa86a8540bb9a170b005b967f
Your Name <you@example.com>
2021-04-17 17:22:13 +0700

    first cmm

--- src/main/java/group2it81/configs/HibernateUtils.java
+++ src/main/java/group2it81/configs/HibernateUtils.java
@@ -1,6 +1,5 @@
 package group2it81.configs;
 
-
 import java.util.Properties;
 
 import org.hibernate.SessionFactory;
@@ -22,17 +21,16 @@ import group2it81.pojo.NhanVien;
 
 public class HibernateUtils {
     private static final SessionFactory FACTORY;
-    
+
     static {
         Configuration conf = new Configuration();
-        
-        
+
         Properties props = new Properties();
         props.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
         props.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
         props.put(Environment.URL, "jdbc:mysql://localhost:3306/bookstoredb");
         props.put(Environment.USER, "root");
-        props.put(Environment.PASS, "12345");
+        props.put(Environment.PASS, "123456");
         props.put(Environment.SHOW_SQL, "true");
 
         conf.addAnnotatedClass(User.class);
@@ -40,14 +38,13 @@ public class HibernateUtils {
         conf.addAnnotatedClass(NhanVien.class);
 
         conf.setProperties(props);
-        
-        ServiceRegistry registry = new StandardServiceRegistryBuilder()
-                .applySettings(conf.getProperties()).build();
-        
+
+        ServiceRegistry registry = new StandardServiceRegistryBuilder().applySettings(conf.getProperties()).build();
+
         FACTORY = conf.buildSessionFactory(registry);
     }
 
-    public static final SessionFactory getSessionFactory(){
+    public static final SessionFactory getSessionFactory() {
         return FACTORY;
     }
 }
